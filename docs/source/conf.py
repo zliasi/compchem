ea6668989c8c99c50d8a37d411b62d4651801027
zliasi <liasi@tutanota.com>
2025-10-01 16:08:57 +0200

    Sphinx modification

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,9 +11,9 @@ release = '0.1.0'
 
 # General configuration
 extensions = [
-    'myst_parser',  # Markdown support
-    'sphinx.ext.mathjax',  # Math support
-    'sphinx.ext.githubpages',  # GitHub Pages support
+    'myst_parser',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.githubpages',
 ]
 
 # Markdown support
@@ -23,7 +23,7 @@ source_suffix = {
 }
 
 myst_enable_extensions = [
-    "colon_fence",  # ::: instead of ```
+    "colon_fence",
     "deflist",
     "html_image",
 ]
@@ -32,24 +32,20 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 # HTML output options
-#html_theme = 'sphinx_rtd_theme'  
-
-#html_theme_options = {
-#    'navigation_depth': 3,
-#    'collapse_navigation': False,
-#}
-
 html_theme = 'pydata_sphinx_theme'
 
 html_theme_options = {
-    "github_url": "https://github.com/zliasi/compchem",
-    "show_nav_level": 2,
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/zliasi/compchem",
+            "icon": "fab fa-github-square",
+        },
+    ],
+    "navbar_end": ["navbar-icon-links"],
+    "show_toc_level": 2,
     "navigation_depth": 3,
 }
 
-
 html_static_path = ['_static']
 html_title = "Introduction to computational chemistry"
-
-# GitHub Pages: create .nojekyll file
-html_extra_path = ['.nojekyll']
